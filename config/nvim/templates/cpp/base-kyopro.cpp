873f22b90452be64762971f33653db06ffc68c45
odan <odan3240@gmail.com>
2017-04-15 18:43:26 +0000

    add templates

---
+++ config/nvim/templates/cpp/base-kyopro.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(c) (c).begin(), (c).end()
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int main() { 
+    {{_cursor_}}
+    return 0; 
+}
