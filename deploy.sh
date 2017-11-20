a8136e2d895d225907411259190bbd297ce0ada5
odan <odan3240@gmail.com>
2017-11-20 23:43:39 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-ln -sf $PWD/bashrc $HOME/.bash_aliases
-ln -sf $PWD/vimrc $HOME/.vimrc
-ln -sf $PWD/vim $HOME/.vim
-ln -sf $PWD/bash_powerline.sh  $HOME/.bash_powerline.sh
-ln -sf $PWD/tmux.conf $HOME/.tmux.conf
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > ~/install.sh
-sh ~/install.sh
+mkdir -p $HOME/.config
+ln -sfhv $PWD/config/dein $HOME/.config/
+ln -sfhv $PWD/config/nvim $HOME/.config/
+ln -sfhv $PWD/tmux.conf $HOME/.tmux.conf
+ln -sfhv$PWD/zshrc $HOME/.zshrc
