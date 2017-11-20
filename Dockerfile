5085a8c4689021329b0dc27d6dada4997b335152
odan <odan3240@gmail.com>
2017-11-21 00:59:18 +0900

    add pip3

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu
 
 ENV LANG C.UTF-8
 RUN apt update
-RUN apt install -y curl git zsh python-pip gawk locales
+RUN apt install -y curl git zsh python-pip python3-pip gawk locales
 RUN locale-gen ja_JP.UTF-8
 
 WORKDIR /root
--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #/bin/sh
 
 pip install -r requirements.txt
+if type "pip3" > /dev/null 2>&1
+then
+    pip3 install -r requirements.txt
+fi
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
