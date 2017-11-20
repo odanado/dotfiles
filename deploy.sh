cf00dc4633fb47fcf247d448ccef62693c90e81c
odan <odan3240@gmail.com>
2017-11-21 01:59:42 +0900

    fix powerline config

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ ln -snfv $PWD/config/dein $HOME/.config/
 ln -snfv $PWD/config/nvim $HOME/.config/
 
 ln -snfv $PWD/config/powerline/config.json $HOME/.config/powerline/
-ln -snfv $PWD/config/powerline/themes $HOME/.config/powerline/
+ln -snfv $PWD/config/powerline/themes $HOME/.config/powerline
 
 ln -snfv $PWD/gitconfig $HOME/.gitconfig
 
