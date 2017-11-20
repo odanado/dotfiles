acedb7a478ab096005b75e501a5e8dc2a5ab7cb9
odan <odan3240@gmail.com>
2017-11-21 00:57:40 +0900

    add locale of Docker

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM ubuntu
 
 ENV LANG C.UTF-8
 RUN apt update
-RUN apt install -y curl git zsh python-pip gawk
+RUN apt install -y curl git zsh python-pip gawk locales
+RUN locale-gen ja_JP.UTF-8
 
 WORKDIR /root
 RUN mkdir /root/dotfiles
--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ mkdir -p $HOME/.config
 ln -snfv $PWD/config/dein $HOME/.config/
 ln -snfv $PWD/config/nvim $HOME/.config/
 ln -snfv $PWD/tmux.conf $HOME/.tmux.conf
-ln -snfv $PWD/zsh $HOME/.zsh
 
+ln -snfv $PWD/zsh/zshrc $HOME/.zshrc
+ln -snfv $PWD/zsh/zshrc.darwin $HOME/.zshrc.darwin
 ln -snfv $PWD/zsh/zshenv $HOME/.zshenv
