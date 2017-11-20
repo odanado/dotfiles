57153227e806e2f58fb1a87e91e40fdd7329e209
odan <odan3240@gmail.com>
2017-11-21 00:02:07 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ mkdir -p $HOME/.config
 ln -sfhv $PWD/config/dein $HOME/.config/
 ln -sfhv $PWD/config/nvim $HOME/.config/
 ln -sfhv $PWD/tmux.conf $HOME/.tmux.conf
-ln -sfhv$PWD/zshrc $HOME/.zshrc
+ln -sfhv $PWD/zsh $HOME/.zsh
