60acfb16c3a5e2772ea49a5e3ad0f7ab53146aff
Kita Tomoya <kita@mfac.jp>
2018-06-12 11:43:19 +0900

    add gvm

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,6 @@
 source $HOME/.zplug/init.zsh
 
 zplug check || zplug install
+
+echo "install gvm"
+zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
