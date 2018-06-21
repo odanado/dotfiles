6c52c78681b6593e310b237c4cc664332dee6111
Kita Tomoya <kita@mfac.jp>
2018-06-22 01:19:20 +0900

    add ndenv

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,13 @@ source $HOME/.zplug/init.zsh
 
 zplug check || zplug install
 
-echo "install gvm"
-zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+if [ ! -s "$HOME/.gvm/scripts/gvm" ]; then
+    echo "install gvm"
+    zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+fi
+
+if [ ! -s "$HOME/.ndenv" ]; then
+    echo "install ndenv"
+    git clone https://github.com/riywo/ndenv ~/.ndenv
+    git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
+fi
