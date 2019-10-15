4a18c07a845d4b31255f0bf80a86d24e4b66b2b0
odan <odan3240@gmail.com>
2019-10-16 03:32:43 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -eu
 
 # ansible-playbook --verbose -i ansible-playbook/local ansible-playbook/ubuntu.yml
 ansible-playbook --verbose -i ansible-playbook/local ansible-playbook/mac.yml
-zsh -ic "zplug check || zplug install" > /dev/null 2>&1
+# zsh -ic "zplug check || zplug install" > /dev/null 2>&1
