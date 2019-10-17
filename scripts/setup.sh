fb6db7e6092e3025c5b6bff64d74fe119e840006
odan <odan3240@gmail.com>
2019-10-18 02:43:36 +0900

    不要なファイルを削除

--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-pip install --user -r requirements.txt
-if type "pip3" > /dev/null 2>&1
-then
-    pip3 install --user -r requirements.txt
-fi
-
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
