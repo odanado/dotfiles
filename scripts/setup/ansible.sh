ce51dd956ba03cabfb7012574fab0bf6e337e9a2
odan <odan3240@gmail.com>
2020-01-12 20:55:29 +0900

    add playbook for mac ci

--- scripts/setup/ansible.sh
+++ scripts/setup/ansible.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-set -eu
+set -e
 
 os=$(uname -s | tr '[A-Z]' '[a-z]')
 
 case $os in
     darwin)
-        ansible-playbook --verbose -i ansible-playbook/local ansible-playbook/mac.yml
+        if [ -v $GITHUB_WORKFLOW ]; then
+            PLAYBOOK=ansible-playbook/mac-ci.yml
+        else
+            PLAYBOOK=ansible-playbook/mac.yml
+        fi
     ;;
     linux)
-        ansible-playbook --verbose -i ansible-playbook/local ansible-playbook/ubuntu.yml
+        PLAYBOOK=ansible-playbook/ubuntu.yml
     ;;
 esac
+
+ansible-playbook --verbose -i ansible-playbook/local $PLAYBOOK
