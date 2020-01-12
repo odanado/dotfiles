a76946cdc84f7b8b1a7eeda16e81bb15f6a93871
odan <odan3240@gmail.com>
2020-01-12 21:20:45 +0900

    条件が逆だった

--- scripts/setup/ansible.sh
+++ scripts/setup/ansible.sh
@@ -6,16 +6,17 @@ os=$(uname -s | tr '[A-Z]' '[a-z]')
 
 case $os in
     darwin)
-        if [ -v $GITHUB_WORKFLOW ]; then
-            PLAYBOOK=ansible-playbook/mac-ci.yml
-        else
+        if [ -z "$GITHUB_WORKFLOW" ] && [ "${GITHUB_WORKFLOW:-A}" = "${GITHUB_WORKFLOW-A}" ]; then
             PLAYBOOK=ansible-playbook/mac.yml
+        else
+            PLAYBOOK=ansible-playbook/mac-ci.yml
         fi
     ;;
     linux)
         PLAYBOOK=ansible-playbook/ubuntu.yml
     ;;
 esac
+
 
 echo "run playbook: $PLAYBOOK"
 ansible-playbook --verbose -i ansible-playbook/local $PLAYBOOK
