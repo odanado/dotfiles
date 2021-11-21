74bccce278db0fa152c1950cc58f1259a31a79cd
odan <odan3240@gmail.com>
2021-11-21 13:46:05 +0900

    ベンチマークを追加

---
+++ scripts/benchmark.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+rm -f result.txt
+
+
+os=$(uname -s | tr '[A-Z]' '[a-z]')
+
+case $os in
+    darwin)
+        TIME_COMMAND=gtime
+    ;;
+    linux)
+        TIME_COMMAND=time
+    ;;
+esac
+
+{ for i in $(seq 1 10); do $TIME_COMMAND --format="%e" zsh -i -c exit; done } 2> result.txt
+
+
+RESULT=$(cat result.txt | awk '{ total += $1 } END { print total/NR }')
+
+cat<<EOJ
+[
+    {
+        "name": "zsh load time",
+        "unit": "Second",
+        "value": ${RESULT}
+    }
+]
+EOJ
