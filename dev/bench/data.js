5ba6e55c153c1ed6a50b42d74d97a0b205ddb8e8
github-action-benchmark <github@users.noreply.github.com>
2021-11-21 05:51:25 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 132352c05b53bac7ca6767114c839f9cd8560028

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1637473885029,
+  "repoUrl": "https://github.com/odanado/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "odan3240@gmail.com",
+            "name": "odan",
+            "username": "odanado"
+          },
+          "committer": {
+            "email": "odan3240@gmail.com",
+            "name": "odan",
+            "username": "odanado"
+          },
+          "distinct": true,
+          "id": "132352c05b53bac7ca6767114c839f9cd8560028",
+          "message": "ベンチマーク用の yml を分ける",
+          "timestamp": "2021-11-21T14:43:25+09:00",
+          "tree_id": "d4f17cf1208e574e7614fef03af269fb76668b8d",
+          "url": "https://github.com/odanado/dotfiles/commit/132352c05b53bac7ca6767114c839f9cd8560028"
+        },
+        "date": 1637473883637,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time",
+            "value": 1.511,
+            "unit": "Second"
+          }
+        ]
+      }
+    ]
+  }
+}
