ca8e237fcbee4bc5d32d678ed81d54a6ba568264
odan <odan3240@gmail.com>
2021-12-03 22:14:27 +0900

    ci かどうかで挙動を変えるのはやめる

--- scripts/setup/ansible.sh
+++ scripts/setup/ansible.sh
@@ -2,21 +2,7 @@
 
 set -e
 
-os=$(uname -s | tr '[A-Z]' '[a-z]')
-
-case $os in
-    darwin)
-        if [ -z "$GITHUB_WORKFLOW" ] && [ "${GITHUB_WORKFLOW:-A}" = "${GITHUB_WORKFLOW-A}" ]; then
-            PLAYBOOK=ansible-playbook/mac.yml
-        else
-            PLAYBOOK=ansible-playbook/mac-ci.yml
-        fi
-    ;;
-    linux)
-        PLAYBOOK=ansible-playbook/ubuntu.yml
-    ;;
-esac
-
+PLAYBOOK=ansible-playbook/mac.yml
 
 echo "run playbook: $PLAYBOOK"
 ansible-playbook --verbose -i ansible-playbook/local $PLAYBOOK
