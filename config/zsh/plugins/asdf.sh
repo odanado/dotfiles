2eb913e3c9c31354d935b5d76c9eafcfdcd47742
odan <odan3240@gmail.com>
2023-05-13 14:05:23 +0900

    fix

--- config/zsh/plugins/asdf.sh
+++ config/zsh/plugins/asdf.sh
@@ -2,6 +2,6 @@ include () {
   [[ -f "$1" ]] && source "$1"
 }
 
-include "$HOME/.asdf/asdf.sh"
+include "$ASDF_DATA_DIR/asdf.sh"
 
 type -p brew >/dev/null && . $(brew --prefix asdf)/libexec/asdf.sh
--- scripts/link-files.sh
+++ scripts/link-files.sh
@@ -6,4 +6,4 @@ ln -sf $PWD/files/.gitconfig $HOME/.gitconfig
 ln -sf $PWD/files/.gitignore $HOME/.gitignore
 ln -sf $PWD/files/.zshenv $HOME/.zshenv
 
-ln -sf $PWD/config/ $HOME/.config
+ln -sf $PWD/config/* $HOME/.config
