6445d2f3e81ceee73cc52ba836099a11905dca7a
odan <odan3240@gmail.com>
2023-05-17 23:25:07 +0900

    add scripts/install-tools-macos.sh

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -2,12 +2,5 @@
 
 set -eux
 
-curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer
-echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c
-chmod +x aqua-installer
-./aqua-installer
-rm aqua-installer
-
-zsh -c "aqua i"
-
-zsh -c "git clone https://github.com/asdf-vm/asdf.git \$ASDF_DATA_DIR --branch v0.11.3"
+test "$(uname)" = "Darwin" && ./scripts/install-tools-macos.sh
+test "$(uname)" = "Linux" && ./scripts/install-tools-ubuntu.sh
