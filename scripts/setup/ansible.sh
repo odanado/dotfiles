686406d9c707c17cf1dbe05549dafbed773efc07
odan <odan3240@gmail.com>
2023-05-19 01:59:15 +0900

    不要なファイルを削除した

--- scripts/setup/ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-PLAYBOOK=ansible-playbook/mac.yml
-
-echo "run playbook: $PLAYBOOK"
-ansible-playbook -i ansible-playbook/local $PLAYBOOK
--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-./scripts/setup/ansible.sh
