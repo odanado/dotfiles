10d0c4a96fa45a3343fda4628ab5c57e85771300
odan <odan3240@gmail.com>
2024-09-23 22:48:00 +0900

    update completion method

--- config/zsh/plugins/completion.zsh
+++ config/zsh/plugins/completion.zsh
@@ -4,5 +4,6 @@ include () {
 
 type -p brew >/dev/null && include $(brew --caskroom)/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 type -p aqua >/dev/null && eval "$(aqua completion zsh)"
+type -p gibo >/dev/null && eval "$(gibo completion zsh)"
 
 eval "$(direnv hook zsh)"
