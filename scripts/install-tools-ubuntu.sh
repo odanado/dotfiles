4e27b494568106707d87549c3f6dd359ad7bc388
odan <odan3240@gmail.com>
2024-09-23 22:09:59 +0900

    update aqua install step

--- scripts/install-tools-ubuntu.sh
+++ scripts/install-tools-ubuntu.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer
-echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c
+curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
+echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  aqua-installer" | sha256sum -c
 chmod +x aqua-installer
 ./aqua-installer
 rm aqua-installer
