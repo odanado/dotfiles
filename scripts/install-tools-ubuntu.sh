c824d7fbd7daba146fdb29e6795f5aa4acb159cc
odan <odan3240@gmail.com>
2025-11-22 16:38:11 +0900

    fix scripts/install-tools-ubuntu.sh

--- scripts/install-tools-ubuntu.sh
+++ scripts/install-tools-ubuntu.sh
@@ -2,12 +2,19 @@
 
 set -eux
 
-curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
-echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  aqua-installer" | sha256sum -c
-chmod +x aqua-installer
-./aqua-installer
-rm aqua-installer
+# curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
+# echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  aqua-installer" | sha256sum -c
+# chmod +x aqua-installer
+# ./aqua-installer
+# rm aqua-installer
 
-zsh -c "aqua i"
+# zsh -c "aqua i"
 
 curl https://mise.run | sh
+
+mise use -g github:cli/cli
+mise use -g github:peco/peco
+mise use -g github:peco/peco
+mise use -g github:rossmacarthur/sheldon
+mise use -g github:starship/starship
+mise use -g github:direnv/direnv
