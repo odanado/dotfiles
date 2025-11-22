9d337b8b2882493d6afccde2594e611ca0111ff4
odan <odan3240@gmail.com>
2025-11-22 20:41:04 +0900

    fix

--- scripts/install-tools-ubuntu.sh
+++ scripts/install-tools-ubuntu.sh
@@ -12,4 +12,8 @@ set -eux
 
 curl https://mise.run | sh
 
-mise i --env ubuntu --verbose
+mise use -g github:cli/cli
+mise use -g github:peco/peco
+mise use -g github:rossmacarthur/sheldon
+mise use -g github:starship/starship
+mise use -g github:direnv/direnv[exe=direnv]
